fb009c30affc2800898bb0e608610a1e559c0321
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2024-02-22 14:57:55 +0100

    more testing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
+function colDef(fieldName) {
+  const def = { field: fieldName };
+  if (fieldName == "Id") {
+    def.type = "idColumn";
+  }
+  if (fieldName.includes("(DATE)")) {
+    def.type = "dateColumn";
+  }
+  return def;
+}
+
+function row(unsplit) {
+  const row = {};
+  for (key in unsplit) {
+    row[key] = unsplit[key];
+    //row[key] = unsplit[key].split(",");
+  }
+  return row;
+}
+
 async function showGrid() {
   const response = await fetch("../dataset.zip");
   const zipBlob = await response.blob();
@@ -11,13 +31,30 @@ async function showGrid() {
   if (parsed.errors) {
     console.log("parse errors", parsed.errors);
   }
-  const rows = parsed.data;
+  const rows = parsed.data.map(row);
   const keys = Object.keys(rows[0]);
-  const fields = keys.map((key) => ({ field: key }));
-  //console.log(fields)
-  const columnDefs = fields;
-  const gridOptions = { columnDefs, rowData: rows };
-
+  const columnDefs = keys.map(colDef);
+  console.log(columnDefs);
+  const defaultColDef = {
+    editable: false,
+    sortable: true,
+    filter: "agNumberColumnFilter",
+  };
+  const columnTypes = {
+    idColumn: { filter: "agTextColumnFilter" },
+    dateColumn: {
+      filter: "agDateColumnFilter",
+      cellDataType: "dateString",
+    },
+  };
+  const gridOptions = {
+    columnDefs,
+    rowData: rows,
+    defaultColDef,
+    columnTypes,
+    enableCellTextSelection: true,
+  };
+  console.log(gridOptions);
   const myGridElement = document.querySelector("#myGrid");
   agGrid.createGrid(myGridElement, gridOptions);
 }
